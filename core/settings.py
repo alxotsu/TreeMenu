104f4c0d612f5ddf210b5e4b95e16e9745fcb7ec
AlxOtsu <workunit1368@gmail.com>
2023-05-09 18:12:09 +0300

    init commit

---
+++ core/settings.py
@@ -0,0 +1,99 @@
+from os import environ
+from pathlib import Path
+
+
+def get_env(name, raise_key_error=False, default=None):
+    try:
+        value = environ[name]
+    except KeyError:
+        if raise_key_error and default is None:
+            raise KeyError(f"The environment don't contain the required value: {name}")
+        else:
+            return default
+    if value.isdigit():
+        return int(value)
+    if value.lower() == "true":
+        return True
+    if value.lower() == "false":
+        return False
+    return value
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+###
+# general
+SECRET_KEY = get_env("SECRET_KEY", raise_key_error=True)
+DEBUG = get_env("DEBUG", default=False)
+ALLOWED_HOSTS = ['*']
+ROOT_URLCONF = 'core.urls'
+STATIC_URL = "static/"
+# general
+###
+
+###
+# apps & middlewares
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+]
+# apps & middlewares
+###
+
+###
+# database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# database
+###
+
+###
+# other
+WSGI_APPLICATION = 'core.wsgi.application'
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# other
+###
