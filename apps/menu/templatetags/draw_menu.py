4d63fce83ef1430efcc2e38bfbfd279f6a7509fb
AlxOtsu <workunit1368@gmail.com>
2023-05-10 03:25:47 +0300

    replace html template tag on django template tags (Внезапно понял, о каких тегах идёт речь в задании)

--- apps/menu/templatetags/draw_menu.py
+++ apps/menu/templatetags/draw_menu.py
@@ -7,16 +7,20 @@ from apps.menu.models import MenuItem
 register = template.Library()
 
 
-@register.inclusion_tag("menu/menu_template.html")
+@register.inclusion_tag("menu/draw/menu_template.html")
 def draw_menu(menu_name, active_item_name=None):
     menu_items = MenuItem.objects.select_related('parent', 'menu').filter(menu__name=menu_name)
     if not menu_items.exists():
         raise ObjectDoesNotExist(f"The Menu \"{menu_name}\" does not exist or not contain items")
     hierarchy = get_menu_hierarchy(menu_items, active_item_name)
-    s_hierarchy = str(hierarchy).replace("'", '"')
     return {
-        "hierarchy": s_hierarchy,
+        "hierarchy": hierarchy,
         "menu_name": menu_name,
         "active_item_name": active_item_name,
         "menu_url": menu_items[0].menu.get_url()
     }
+
+
+@register.inclusion_tag("menu/draw/menu_template_recursive.html")
+def _draw_menu_recursive(item, active_item_name, menu_name):
+    return {"item": item, "active_item_name": active_item_name, "menu_name": menu_name}
